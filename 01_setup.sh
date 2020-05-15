afadfcd7020e8a6873d566cd64f82337ed5bd63a
Ricardo Oliveira <ricardo@hashicorp.com>
2020-05-15 16:44:30 +0100

    waiting for mesh gateway added and reduced number of consul servers

--- 01_setup.sh
+++ 01_setup.sh
@@ -9,7 +9,7 @@ helm install cluster-1 hashicorp/consul -f values1.yaml \
     --version $CONSUL_HELM_VERSION \
     --set global.datacenter=cluster-1 \
     --wait
-kubectl wait --for=condition=available --timeout=1m deployment.apps/consul-connect-injector-webhook-deployment
+kubectl wait --for=condition=available --timeout=1m deployment.apps/consul-mesh-gateway
 
 kubectl get secret consul-federation -o yaml > consul-federation-secret.yaml
 
@@ -20,10 +20,11 @@ helm install cluster-2 hashicorp/consul -f values2.yaml \
     --set global.datacenter=cluster-2 \
     --wait
 
-kubectl wait --for=condition=available --timeout=1m deployment.apps/consul-connect-injector-webhook-deployment
+kubectl wait --for=condition=available --timeout=1m deployment.apps/consul-mesh-gateway
 
 c1_kctl apply -f apps_cli/
 c2_kctl apply -f apps_cli/counting-dashboard.yaml
 
+c1_kctx
 minikube service list -p cluster-1
 minikube service list -p cluster-2
