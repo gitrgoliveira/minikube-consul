133692f2fec5d697296882f9c169b456550d923a
Ricardo Oliveira <ricardo@hashicorp.com>
2020-07-21 19:41:57 +0100

    minikube broke the hyperkit network bit. changing to virtualbox made it work.
    I'll need to investigate this further, because virtualbox is too heavy.

--- 00_start.sh
+++ 00_start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 source helper.sh
 
-minikube start -p cluster-1 --driver=hyperkit
-minikube start -p cluster-2 --driver=hyperkit
+minikube start -p cluster-1 --vm=true --driver=virtualbox
+minikube start -p cluster-2 --vm=true --driver=virtualbox
 
 eval $(minikube docker-env -p cluster-1)
 docker build --rm webapp_v1/ -t webapp:v1
