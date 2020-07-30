ddc0efa045bc1c994b83a2bd0ed0ef699b5f13c9
Ricardo Oliveira <ricardo@hashicorp.com>
2020-07-30 10:19:54 +0200

    trying to make minikube a bit more stable.

--- 00_start.sh
+++ 00_start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source helper.sh
 
-minikube start -p cluster-1 --vm=true --driver=hyperkit
+minikube start -p cluster-1 --vm=true --driver=hyperkit --cpus=4
 minikube start -p cluster-2 --vm=true --driver=hyperkit
 
 eval $(minikube docker-env -p cluster-1)
